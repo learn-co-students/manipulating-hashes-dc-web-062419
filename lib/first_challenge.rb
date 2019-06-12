5743a64001c087108263230cbf9cdaf0c02fa0db
Michelle Lam <shelly2.lam@gmail.com>
2019-06-11 23:23:31 -0500

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,13 @@ def first_challenge
   }
 
   #your code here
-
-
-  #remember to return your newly altered contacts hash!
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+        value.delete_if {|value| value == "strawberry"}
+      end
+      end
+  end #remember to return your newly altered contacts hash!
   contacts
 end
 
